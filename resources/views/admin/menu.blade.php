56021ff5153abcff360253f29d82889d5020cc48
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2020-02-25 20:25:45 +0000

    setup and styled the main admin menu

--- resources/views/admin/menu.blade.php
+++ resources/views/admin/menu.blade.php
@@ -3,5 +3,19 @@
 @section('title', 'Menu')
 
 @section('content')
-    <h1>Stuff</h1>
+    <div id="menuHeader">
+        <h2 id="menuTitle">MD Portfolio</h2>
+        <h6 id="menuSubTitle">Administration Menu</h6>
+    </div>
+    <!-- <hr /> -->
+    
+    <!-- Menu items -->
+    <!-- Bootstrap-wise, need to use a div if you want the full bar of the items to be clickable -->
+    <div class="list-group">
+        <a href="/admin/users" class="list-group-item list-group-item-action">Users</a>
+        <a href="/admin/logos" class="list-group-item list-group-item-action">Logos</a>
+        <a href="/admin/tools" class="list-group-item list-group-item-action">Tools</a>
+        <a href="/admin/projects" class="list-group-item list-group-item-action">Projects</a>
+        <a href="/admin/aboutlinks" class="list-group-item list-group-item-action">About Links</a>
+    </div>
 @endsection
