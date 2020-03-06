126c7ea0c1ead911f7bb7e86bef243aad73338d1
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2020-03-06 18:40:28 +0000

    the logoSelector will now always auto-select the first item, meaning you cannot submit without a logo selected

--- resources/views/admin/partials/logoSelector.blade.php
+++ resources/views/admin/partials/logoSelector.blade.php
@@ -1,16 +1,21 @@
 <label for="selectedLogoID">Select a Logo:</label>
 <div id="logoSelector">
     @if(isset($logos) && count($logos) > 0)
-        @foreach($logos as $logo)
+        @for($i = 0; $i < count($logos); $i++)
+            
+            @php 
+                $logo = $logos[$i]; 
+            @endphp
+            
             <img 
-                class="img-fluid" 
+                class="img-fluid {{ ($i == 0) ? 'selectedLogo' : '' }}" 
                 id="logoID:{{ $logo['id'] }}" 
                 src="{{ $logo['url'] }}" 
                 alt="Logo: {{ $logo['name'] }}" 
                 onClick="onLogoClick({{ $logo['id'] }})" 
             />
-        @endforeach
-        <input type="hidden" id="selectedLogoID" name="selectedLogoID" value="" /> <!-- Is set by the below script -->
+        @endfor
+        <input type="hidden" id="selectedLogoID" name="selectedLogoID" value="{{ $logos[0]['id'] }}" /> <!-- Is changed by the below script -->
     @else
         <p>There are currently no logos stored in the database.</p>
     @endif
