dc4305c3bb9982a98d8b76d7149b3e7cc38edb44
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2020-03-07 10:08:21 +0000

    setup the password change view

---
+++ resources/views/admin/users/passwordChange.blade.php
@@ -0,0 +1,74 @@
+@extends('layouts.admin')
+
+@php
+    $pageTitle = 'Change my Password';
+    $menuURL = "test/menu";
+    $id = 3;
+@endphp
+
+@section('title', $pageTitle)
+
+@section('content')
+    
+    <h1>{{ $pageTitle }}:</h1>
+    
+    @include('admin.partials.backToMenuBtn')
+    
+    <form 
+        action="/admin/users/changepassword/{{ $id }}" 
+        method="post" 
+    >
+        @csrf
+        
+        @method('PATCH')
+        
+        <div class="form-group">
+            
+            @include('admin.partials.inputContainer', [
+                'inputName' => 'currentPasswordInput',
+                'inputLabel' => "Current Password:",
+                'inputErrorName' => 'currentPasswordError',
+                'inputField' => '<input 
+                    type="password" 
+                    class="form-control" 
+                    name="currentPasswordInput" 
+                    id="currentPasswordInput" 
+                    value="" 
+                    required 
+                />'
+            ])
+            
+            @include('admin.partials.inputContainer', [
+                'inputName' => 'newPasswordInput',
+                'inputLabel' => "New Password:",
+                'inputErrorName' => 'newPasswordError',
+                'inputField' => '<input 
+                    type="password" 
+                    class="form-control" 
+                    name="newPasswordInput" 
+                    id="newPasswordInput" 
+                    value="" 
+                    required 
+                />'
+            ])
+            
+            @include('admin.partials.inputContainer', [
+                'inputName' => 'newPasswordInput-confirm',
+                'inputLabel' => "Confirm New Password:",
+                'inputErrorName' => 'newPasswordError',
+                'inputField' => '<input 
+                    type="password" 
+                    class="form-control" 
+                    name="newPasswordInput-confirm" 
+                    id="newPasswordInput-confirm" 
+                    value="" 
+                    required 
+                />'
+            ])
+            
+        </div>
+        
+        <button type="submit" class="btn btn-primary">Save</button>
+    </form>
+    
+@endsection
