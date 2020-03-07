a5e1674d54b1461477d7edf586f214093c1d240b
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2020-03-07 11:34:22 +0000

    corrected issue in inputContainer partial where an $errorName is always required, now doesn't require one to be given

--- resources/views/admin/partials/inputContainer.blade.php
+++ resources/views/admin/partials/inputContainer.blade.php
@@ -1,5 +1,13 @@
 <div class="inputContainer">
     <label for="{{ $inputName }}">{{ $inputLabel }}</label>
     {!! $inputField !!}
+    
+    @php
+        if(!isset($inputErrorName))
+        {
+            $inputErrorName = "";
+        }
+    @endphp
+    
     @include('admin.partials.inputError', ['errorName' => $inputErrorName])
 </div>
