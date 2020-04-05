64d96f78b6743e6c10db920eb20ecea34e0a1328
Matthew Stuart Derbyshire <mderbyshiredeveloper@gmail.com>
2020-04-05 12:11:16 +0100

    created migration for the subjects table

---
+++ database/migrations/2020_04_05_110500_create_subjects_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subjects', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('profession');
+            $table->string('why_top');
+            $table->string('why_bottom');
+            $table->string('email');
+            $table->string('phone');
+            $table->timestamps();
+            $table->unsignedBigInteger('lastChangedby');
+            
+            $table->foreign('lastChangedBy')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subjects');
+    }
+}
